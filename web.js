9ccb3e31f342866d7af2e145fcd2710cab3d8766
Igor Rozkov <irozkov@gmail.com>
2013-07-10 10:24:41 +0000

    Added fs.readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require("fs");
+
+var buffer = new Buffer(fs.readFileSync("index.index", "utf8"));
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
